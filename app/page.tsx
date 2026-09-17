6eac594e4560f7eb58ecda40ff960e3aaf8fbb56
karthikeyan <113494526+Karthi7170@users.noreply.github.com>
2026-09-17 18:15:53 +0530

    Add resume generator to VIP-Hunter dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import JobDashboard from "@/components/JobDashboard";
 import ExternalJobSearch from "@/components/ExternalJobSearch";
 import AutoApplyQueue from "@/components/AutoApplyQueue";
+import ResumeGenerator from "@/components/ResumeGenerator";
 import { jobs } from "@/lib/jobs";
 
 export default function Home() {
   return (
     <>
       <JobDashboard jobs={jobs} />
+      <ResumeGenerator />
       <ExternalJobSearch />
       <AutoApplyQueue />
     </>
